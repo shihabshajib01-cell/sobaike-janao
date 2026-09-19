57c6ba8dbcef3397ad21482f8bbfaedc96df01fe
shihabshajib01-cell <shihabshajib01@gmail.com>
2026-09-19 10:14:27 +0600

    forms: allow native input mode overrides on number fields

--- src/components/ui/NumberField.tsx
+++ src/components/ui/NumberField.tsx
@@ -1,17 +1,17 @@
 import React from 'react';
 import { TextField, TextFieldProps } from './TextField';
 
-export interface NumberFieldProps extends Omit<TextFieldProps, 'type' | 'inputMode'> {
+export interface NumberFieldProps extends Omit<TextFieldProps, 'type'> {
   integer?: boolean;
 }
 
 export const NumberField = React.forwardRef<HTMLInputElement, NumberFieldProps>(
-  ({ integer = false, step, ...props }, ref) => (
+  ({ integer = false, step, inputMode, ...props }, ref) => (
     <TextField
       ref={ref}
       {...props}
       type="number"
-      inputMode={integer ? 'numeric' : 'decimal'}
+      inputMode={inputMode ?? (integer ? 'numeric' : 'decimal')}
       step={step ?? (integer ? 1 : 'any')}
     />
   )
