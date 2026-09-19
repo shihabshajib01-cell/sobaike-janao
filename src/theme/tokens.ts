f06a6d84d8a060ea00b7685e74bc7593d5acb348
shihabshajib01-cell <shihabshajib01@gmail.com>
2026-09-19 13:41:52 +0600

    Set hero autoplay interval to 8 seconds

--- src/theme/tokens.ts
+++ src/theme/tokens.ts
@@ -362,7 +362,7 @@ export const HERO_TOKENS = {
 };
 
 export const HERO_SLIDER_BEHAVIOR = {
-  autoplayIntervalMs: 15_000,
+  autoplayIntervalMs: 8_000,
   swipeThresholdPx: 45,
   swipeDominanceRatio: 1.2,
   transitionDurationMs: 500,
